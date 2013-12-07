7b36209d0a5a37065ee217d14fe270345ff7d6c8
Mehdi Mirza <memirzamo@gmail.com>
2013-12-07 01:03:49 -0500

    minor bug fixes

--- experiments/random_search_dropout_maxout_cifar/worker.sh
+++ experiments/random_search_dropout_maxout_cifar/worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $1
-train.py task_0.yaml || exit -1
-echo "starting task 1"
-train.py task_1.yaml
+python ~/projects/pylearn2/pylearn2/scripts/train.py task_0.yaml || exit -1
+#echo "starting task 1"
+#train.py task_1.yaml
