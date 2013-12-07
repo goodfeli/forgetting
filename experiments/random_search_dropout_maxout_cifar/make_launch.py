4d3acaf88696e28697e6c31c571d5a3be30733d7
Mehdi Mirza <memirzamo@gmail.com>
2013-12-06 23:37:26 -0500

    cifar_svhn

---
+++ experiments/random_search_dropout_maxout_cifar/make_launch.py
@@ -0,0 +1,5 @@
+base = """jobdispatch --torque --env=THEANO_FLAGS=device=gpu,floatX=float32,force_device=True --duree=24:00:00 --whitespace --gpu $F/experiments/random_search_dropout_maxout_cifar/worker.sh $F/experiments/random_search_dropout_maxout_cifar/exp/"{{%(args)s}}\""""
+args = ','.join([str(job_id) for job_id in xrange(25)])
+f = open('launch.sh', 'w')
+f.write(base % locals())
+f.close()
