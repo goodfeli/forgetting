321ddbdc8973fbbb2180c1c64a5240509eaab968
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-12-18 12:35:10 -0500

    implemented ppf

--- ppf.py
+++ ppf.py
@@ -15,6 +15,45 @@ def cloud_to_ppf(x, y, higher_better=True):
     curve.
     """
 
-    raise NotImplementedError()
+    print 'running cloud to ppf'
+
+    combined = zip(x, y)
+    s = sorted(combined, key = lambda e : e[0])
+    x = [e[0] for e in s]
+    y = [e[1] for e in s]
+
+    i = 0
+    while i < len(x):
+        lx = x[i]
+        ly = y[i]
+        j = len(x) - 1
+        while j > i:
+            rx = x[j]
+            assert rx > lx
+            ry = y[j]
+            slope = (ry - ly) / (rx - lx)
+            intercept = ly - slope * lx
+            k = i + 1
+
+            while k < j:
+                mx = x[k]
+                my = y[k]
+                pred = slope * mx + intercept
+                if (higher_better and my <= pred) or (my >= pred and not
+                        higher_better):
+                    del x[k]
+                    del y[k]
+                    j -= 1
+                else:
+                    k += 1
+            j -= 1
+        i += 1
+
+
+
+
+    print 'done running cloud to pff'
+
+    return x, y
 
 
