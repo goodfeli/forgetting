42042496a44c48073babbc20988a616512e7da2d
Mehdi Mirza <memirzamo@gmail.com>
2013-12-17 19:54:43 -0500

    sgd_relu_mnist_amazon

---
+++ experiments/random_search_sgd_relu_mnist_amazon/local_launch.sh
@@ -0,0 +1,5 @@
+for fold in 1 2 3 4 5 6 7 8 8 9 10 11 12
+    do
+        $SCRATCH/results/forgetting/random_search_dropout_maxout_mnist_amazon/worker.sh $SCRATCH/results/forgetting/random_search_dropout_maxout_mnist_amazon/exp/$fold
+    done
+
