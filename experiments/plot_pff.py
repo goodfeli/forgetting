0a7bd1d2de3557825be57dd077e074228f2563af
memimo <memirzamo@gmail.com>
2013-12-19 17:50:28 -0600

    plots pretty-fied

--- experiments/plot_pff.py
+++ experiments/plot_pff.py
@@ -5,28 +5,49 @@ from matplotlib import pyplot as plt
 import numpy as np
 import os
 import sys
+import pickle
 
-from pylearn2.utils import serial
+
 
 plt.hold(True)
 ax = plt.gca()
 ax.set_xscale('log')
 ax.set_yscale('log')
 
-i = 0
 
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '#555555']
 markers = ['d', 'x', '+', 'o', '<', 'v', '^', 's']
 
+data = pickle.load(open(sys.argv[1], 'r'))
 
-data = serial.load(sys.argv[1])
+def dumb_sort(data):
+    sort_keys = ['sgd_sigmoid', 'dropout_sigmoid', 'sgd_relu', 'dropout_relu', 'sgd_maxout', 'dropout_maxout', 'sgd_lwta', 'dropout_lwta']
+    new = []
+    old = []
+    name = []
+    for item in sort_keys:
+        for i in xrange(len(data['name'])):
+                if item in data['name'][i]:
+                    new.append(data['new'][i])
+                    old.append(data['old'][i])
+                    name.append(data['name'][i])
+                    continue
+    return new, old, name
 
-
-for old,  new, main_dir in zip(data['old'], data['new'], data['name']):
+new_, old_, name_ = dumb_sort(data)
+i = 0
+for old,  new, main_dir in zip(old_, new_, name_):
     plt.plot(old, new, label=main_dir, color=colors[i], marker=markers[i])
+    i+=1
 
 
-    i += 1
+# mnist
+#plt.legend(bbox_to_anchor=(0.8,1.))
+# mnist_amazon
+#plt.legend(bbox_to_anchor=(0.87,1.))
+#plt.xlim(0.5e-3,1.)
+# amazon
+plt.legend(bbox_to_anchor=(0.4,1.))
 
-plt.legend()
+
 plt.show()
