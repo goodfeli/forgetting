f09e880d45d48d962fbf2c26ab4a3ccc5e503788
memimo <memirzamo@gmail.com>
2013-12-21 16:20:07 -0600

    plot updated

--- experiments/plot_pff.py
+++ experiments/plot_pff.py
@@ -34,20 +34,52 @@ def dumb_sort(data):
                     continue
     return new, old, name
 
+def convert_name(name):
+    def captalize(name):
+        if name == 'dropout':
+            return 'Dropout'
+        if name == 'sgd':
+            return 'SGD'
+        if name == 'maxout':
+            return 'Maxout'
+        if name == 'relu':
+            return 'ReLUs'
+        if name == 'lwta':
+            return 'LWTA'
+        if name == 'sigmoid':
+            return 'Sigmoid'
+
+    name = name.split('_')
+    return "{}, {}".format(captalize(name[2]), captalize(name[3]))
+
 new_, old_, name_ = dumb_sort(data)
 i = 0
 for old,  new, main_dir in zip(old_, new_, name_):
-    plt.plot(old, new, label=main_dir, color=colors[i], marker=markers[i])
+    plt.plot(old, new, label= convert_name(main_dir), color=colors[i], marker=markers[i])
     i+=1
 
 
 # mnist
-#plt.legend(bbox_to_anchor=(0.8,1.))
+if sys.argv[2] == 'mnist':
+    plt.legend(bbox_to_anchor=(0.8,1.))
+    plt.xlabel('Test error, old task')
+    plt.ylabel('Test error, new task')
+    plt.title('Old task: MNIST, New task: MNIST permutation')
+
 # mnist_amazon
-#plt.legend(bbox_to_anchor=(0.87,1.))
-#plt.xlim(0.5e-3,1.)
+if sys.argv[2] == 'mnist_amazon':
+    plt.xlabel('Test error, old task')
+    plt.ylabel('Test error, new task')
+    plt.title('Old task: MNIST (2,9), New task: Amazon (DVD)')
+    plt.legend(bbox_to_anchor=(0.87,1.))
+    plt.xlim(0.5e-3,1.)
+
 # amazon
-plt.legend(bbox_to_anchor=(0.4,1.))
+if sys.argv[2] == 'amazon':
+    plt.legend(bbox_to_anchor=(0.4,1.))
+    plt.xlabel('Test error, old task')
+    plt.ylabel('Test error, new task')
+    plt.title('Old task: Amazon (Kitchen), New task: Amazon (DVD)')
 
 
 plt.show()
